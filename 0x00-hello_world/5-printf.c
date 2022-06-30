153ef2e884dfc350f3d0aaacf0ea41c037731001
nnazorE <nnazoremmanuel706@gmail.com>
2022-06-30 08:37:03 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,10 @@
-#!/bin/bash
-printf("with proper grammar, but the outcome is a piece of art,\n");
-
-
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
