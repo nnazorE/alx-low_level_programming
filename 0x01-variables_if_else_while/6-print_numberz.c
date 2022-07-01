d74a24de4f3da4a3ba61ed0da859a23e418a4e0a
nnazorE <nnazoremmanuel706@gmail.com>
2022-07-01 06:47:33 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Prints the numbers since 0 to 9 
- *
- * Return: Always (Succ
+ * main - Prints the numbers since 0 to 9
+ * Return: Always (Success)
  */
 
 int main(void)
@@ -11,11 +10,9 @@ int main(void)
 	int n;
 
 	for (n = '0'; n <= '9'; n++)
-		{
-			putchar(n);
-		}
-
-		putchar('\n');
-
-		return (0);
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
 }
