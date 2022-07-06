c30c205e370b182a746f10283891ce4e2397eac0
nnazorE <nnazoremmanuel706@gmail.com>
2022-07-06 17:36:20 +0100

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,28 @@
 #include "main.h"
-
 /**
- * print_sign - Entry function
- * @n: character for sign
- * Return: Always 0.
+ * print_sign - function
+ *
+ * @n: The character to evaluate
+ *
+ * Return: Get 1 if c is positive,
+ * get 0 if it's 0, and
+ * get -1 otherwise
  */
-int print_sign(int n);
+int print_sign(int n)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		return (-1);
-	}
-	else if (n > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	_putchar('0');
-	return (0);
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
