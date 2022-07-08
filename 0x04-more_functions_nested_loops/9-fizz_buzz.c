493456f890603aad2d7d85c164051b4f329be4c1
nnazorE <nnazoremmanuel706@gmail.com>
2022-07-08 09:53:06 -0700

    Tasks 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Buzz 3 of 5
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf(" Buzz");
+
+			else
+			{
+
+			}
+		}
+		printf("\n");
+
+		return (0);
+	}
+}
+
