8924df4971ca5d7e8de4e2b042022fbf685d4e1d
nnazorE <nnazoremmanuel706@gmail.com>
2022-08-01 15:49:32 +0100

    My tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,30 +1,23 @@
-i#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - information about dogs
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
-/**
- * dog_t - Typedef for struct dog
- */
-
-typedef struct dog dog_t;
-
-int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /* _DOG_H_ */
+#endif
